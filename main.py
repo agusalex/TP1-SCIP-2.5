4ef6424e5411e3c69377155e9538acadfacd74f9
agusalex <agustin.c.alexnader@gmail.com>
2020-11-05 20:37:38 -0300

    pip freeze

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print "hello"
+from pyscipopt import Model
+# Create a solver instance.
+model = Model("Example")  # model name is optional
+# Access the methods in the scip.pyx file using the solver/model instance model, e.g.:
+x = model.addVar("x")
+y = model.addVar("y", vtype="INTEGER")
+model.setObjective(x + y)
+model.addCons(2*x - y*y >= 0)
+model.optimize()
+sol = model.getBestSol()
+print("x: {}".format(sol[x]))
+print("y: {}".format(sol[y]))
