ca381079e99b267bbdad153c723edcd9c7141b91
agusalex <agustin.c.alexnader@gmail.com>
2020-11-08 15:39:52 -0300

    progreso

--- ejemplo.zpl
+++
@@ -1,4 +0,0 @@
-set I := 1..10 ;
-do print I;
-do print " Cardinality of I:", card(I);
-do forall <i> in I with i > 5 do print sqrt(i);
--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from pyscipopt import Model
 # Create a solver instance.
 model = Model("Example")  # model name is optional
 # Access the methods in the scip.pyx file using the solver/model instance model, e.g.:
-x = model.addVar("x")
-y = model.addVar("y", vtype="INTEGER")
-model.setObjective(x + y)
-model.addCons(2*x - y*y >= 0)
+
+model.readProblem("ejemplos.zpl")
+#model.setObjective(x + y)
+#model.addCons(2*x - y*y >= 0)
 model.optimize()
 sol = model.getBestSol()
-print("x: {}".format(sol[x]))
-print("y: {}".format(sol[y]))
+print(sol)
+#print("o: {}".format(sol[o]))
+#print("a: {}".format(sol[a]))
+#print("b: {}".format(sol[b]))
+model.writeProblem()
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#model.readProblem("ejemplo.zpl")
